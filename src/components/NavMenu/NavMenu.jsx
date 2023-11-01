d328c2396881ad369e45a68b21921f8811118f6d
Gabriel silva <gabriel23teotonio@gmail.com>
2023-10-31 21:59:50 -0300

    refactor(navMenu): organiza pasta de componte styles

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,42 +1,6 @@
-import { Link, useLocation } from "react-router-dom"
-import styled from "styled-components"
-
-const Container = styled.div`
-  height: 100%;
-  width: 260px;
-  background-color: #020F3D;
-  color: white;
-
-  position: fixed;
-`
-const AreaLogo = styled.div`
-  height: 60px;
-  padding: 10px;
-`
-const Ul = styled.ul`
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-  padding: 10px;
-`
-
-const Li = styled.li`
-  height: 40px;
-`
-
-const StyledLink = styled(Link)`
-  display: flex;
-  border-radius: 4px;
-  align-items: center;
-  padding: 0 10px;
-
-  width: 100%;
-  height: 100%;
-  color: currentColor;
-  text-decoration: none;
-  background: ${props => props.isActive ? "linear-gradient(95deg, #F8104F 5.78%, #D02ED2 44.32%, #2177F8 92.31%)": "none"};
-`
-
+import { useLocation } from "react-router-dom"
+import logo from "../../imgs/logo-energy-laser.png"
+import { AreaLogo, Container, Li, StyledLink, Ul } from "./styles"
 
 export const NavMenu = () => {
   const location = useLocation()
@@ -44,7 +8,7 @@ export const NavMenu = () => {
   return (
     <Container>
         <AreaLogo>
-          <a href="#"><img src="src\imgs\logo-energy-laser.png" alt="logo" /></a>
+          <a href="#"><img src={logo} alt="logo" /></a>
         </AreaLogo>
         <Ul>
           <Li>
