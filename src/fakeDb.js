b4094cd199579141dbf2a29d821702f89651d39b
Gabriel silva <gabriel23teotonio@gmail.com>
2023-11-06 10:16:40 -0300

    feat: valiada cria cliente

---
+++ src/fakeDb.js
@@ -0,0 +1,10 @@
+export const clientes = [
+   { nome: 'João', telefone: '123-456-7890', cpf: '123.456.789-01' },
+   { nome: 'Maria', telefone: '987-654-3210', cpf: '987.654.321-09' },
+   { nome: 'João', telefone: '123-456-7890', cpf: '123.456.789-01' },
+  //  { nome: 'Maria', telefone: '987-654-3210', cpf: '987.654.321-09' },
+  //  { nome: 'João', telefone: '123-456-7890', cpf: '123.456.789-01' },
+  //  { nome: 'Maria', telefone: '987-654-3210', cpf: '987.654.321-09' },
+  //  { nome: 'João', telefone: '123-456-7890', cpf: '123.456.789-01' },
+  //  { nome: 'Maria', telefone: '987-654-3210', cpf: '987.654.321-09' },
+ ];
