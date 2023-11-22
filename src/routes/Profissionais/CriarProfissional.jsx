c073bb66fc484d3f2f0e538d7f7b64a0d160be7a
Gabriel silva <gabriel23teotonio@gmail.com>
2023-11-22 08:27:05 -0300

    feat: cria novo profissional no array ficticio

--- src/routes/Profissionais/CriarProfissional.jsx
+++ src/routes/Profissionais/CriarProfissional.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { FormProfissional } from '../../components/FormProfissional/FormProfissional'
+import { profissionaisDb } from '../../fakeDb'
 
 export const CriarProfissional = () => {
    const createProfissional = (data) => {
-      console.log(data)
+    const {senha, ...prof} = data
+      profissionaisDb.push({...prof, id:profissionaisDb.length + 1})
+      alert('Profissional criado!')
    }
   return (
     <FormProfissional onAction={createProfissional} btnTitle={"Criar profissional"}/>
